6347158b85fbc93997f5880ad4b6c922b49b0b8f
Drodik <drodik@mail.ru>
2023-05-06 23:03:59 +0300

    HW3 is completed

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,36 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(inputString string) []string {
+	if inputString == "" {
+		return []string{}
+	}
+
+	frequencyMap := make(map[string]int)
+
+	elements := strings.Fields(inputString)
+
+	result := make([]string, 0, len(elements))
+
+	for _, item := range elements {
+		if frequencyMap[item] == 0 {
+			result = append(result, item)
+		}
+
+		frequencyMap[item]++
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		if frequencyMap[result[i]] == frequencyMap[result[j]] {
+			return result[i] < result[j]
+		}
+
+		return frequencyMap[result[i]] > frequencyMap[result[j]]
+	})
+
+	return result[:10]
 }
